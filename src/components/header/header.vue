0f656e5a0459e7a881cdb39d061f7d93e0a02c1d
szy1000 <1538078053@qq.com>
2017-05-10 22:27:59 +0800

    finsh header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="header">
+    <div class="background">
+      <img :src="seller.avatar" width="100%">
+    </div>
   	<div class="header-wrapper clearfix">
   		<div class="avatar pull-left">
   			<img width="64" height="64" :src="seller.avatar">
@@ -13,14 +16,23 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="supports">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
+      <div class="support-count" v-if="seller.supports" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="iconfont icon-keyboard_arrow_right"></i>
+      </div>
   	</div>
-  	<div class="bullentin-wrapper">
-  		bullentin
+  	<div class="bulletin-wrapper" @click="showDetail">
+  		<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="iconfont icon-keyboard_arrow_right"></i>
   	</div>
+
+    <div class="detail" v-show="detailShow">
+      
+    </div>
   </div>
 </template>
 
@@ -30,6 +42,19 @@ export default {
     seller: {
       type: Object
     }
+  },
+  data() {
+    return {
+      detailShow: false
+    };
+  },
+  methods: {
+    showDetail() {
+      this.detailShow = true;
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
   }
 };
 </script>
@@ -38,15 +63,53 @@ export default {
   @import "../../common/common.scss";
 
 	.header{
+    position: relative;
     color: $white;
-    background-color: $black;
+    background-color: rgba(7,17,27,.5);
+    overflow: hidden;
+    .background{
+      position: absolute;
+      left: 0;
+      top: 0;
+      height: 137px;
+      width: 100%;
+      filter:blur(10px);
+      z-index: -1;
+    }
 		.header-wrapper{
+      position: relative;
 			padding: 24px 12px 18px 24px;
+      .avatar{
+        img{
+          border-radius: 2px;
+        }
+      }
       .content{
         margin-left: 16px;
       }
+      .support-count{
+        position: absolute;
+        right: 12px;
+        bottom: 14px;
+        padding: 0 8px;
+        height: 24px;
+        line-height: 24px;
+        border-radius: 14px;
+        font-size: 0;
+        background-color: rgba($white,.2);
+        .count{
+          vertical-align: top;
+          font-size: 10px;
+        }
+        i{
+          margin-left: 2px;
+          line-height: 24px;
+          font-size: 10px;
+        }
+      }
     }
     .title{
+      margin-bottom: 8px;
       font-size: 0;
       .brand{
         display: inline-block;
@@ -65,9 +128,83 @@ export default {
         line-height: 18px;
       }
     }
-
-		.bullentin-wrapper{
-
+    .description{
+      margin-bottom: 10px;
+      font-size: 12px;
+      line-height: 12px;
+    }
+    .supports{
+      font-size: 0;
+      .icon{
+        display: inline-block;
+        vertical-align: top;
+        margin-right: 4px;
+        width: 12px;
+        height: 12px;
+        background-size: cover;
+        background-repeat: no-repeat;
+        &.decrease{
+          @include bg-image('decrease_1');
+        }
+        &.discount{
+          @include bg-image('discount_1');
+        }
+        &.guarantee{
+          @include bg-image('guarantee_1');
+        }
+        &.invoice{
+          @include bg-image('invoice_1');
+        }
+        &.special{
+          @include bg-image('special_1');
+        }
+      }
+      .text{
+        line-height: 12px;
+        font-size: 10px;
+      }
+    }
+		.bulletin-wrapper{
+      position: relative;
+      padding: 0 22px 0 12px;
+      height: 28px;
+      line-height: 28px;
+      color: $white;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      background-color: rgba(7,17,27,.2);
+      .bulletin-title{
+        display: inline-block;
+        vertical-align: top;
+        margin-top: 8px;
+        width: 22px;
+        height: 12px;
+        @include bg-image("bulletin");
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+      .bulletin-text{
+        margin: 0 4px;
+        font-size: 10px;
+        line-height: 28px;
+      }
+      i{
+        position: absolute;
+        top: 8px;
+        right: 12px;
+        font-size: 10px;
+      }
 		}
+
+    .detail{
+      position: fixed;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      background-color: rgba(7,17,27,.8);
+    }
 	}
 </style>
